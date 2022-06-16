c1d0a17d4b0d86cb44af078da40dacc7b12ba39a
bainbridge01 <engr.bainbridge@gmail.com>
2022-06-16 01:26:59 -0700

    run seeding & dropdowm fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+5.times do |x|
+    Report.create(item: "shirts",highlights: "extra large", quantity: 2, unit_price: 1350, 
+        sale_price: 3500, client: "Matter", client_phone: 23480141299473,paid: "false")
+end
